a0288a98c803b4a6e28bf40ec68be557d0c08f59
CeliaVargasFdez <celiavargas.fdez@gmail.com>
2022-11-21 13:27:39 +0100

    new folder data_tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "NetAnalyzer"
 require "minitest/autorun"
+
+ROOT_PATH = File.dirname(__FILE__)
+DATA_TEST_PATH = File.join(ROOT_PATH, 'data_tests')
