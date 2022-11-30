5fa24da230f2856b5fa68318f79c1cade02a0774
CeliaVargasFdez <celiavargas.fdez@gmail.com>
2022-11-30 18:00:50 +0100

    tests script ranker_validation.rb

---
+++ test/performancer_test.rb
@@ -0,0 +1,29 @@
+ROOT_PATH = File.dirname(__FILE__)
+require File.join(ROOT_PATH, 'test_helper.rb')
+
+class PerformancerTest < Minitest::Test
+
+	def test_load_control
+		
+	end
+
+	def test_get_pred_rec
+		
+	end
+
+	def test_load_prediction
+		
+	end
+
+	def test_pred_rec
+		
+	end
+
+	def test_get_cuts
+		
+	end
+
+	def test_get_reliable_labels
+		
+	end
+end
